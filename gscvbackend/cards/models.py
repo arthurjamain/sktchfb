720180b76926ece6ce9017d0323f273c1b6ca21d
Arthur Jamain <arthur.jamain@gmail.com>
2014-12-19 12:52:42 +0100

    Added some more interesting properties on the Card model

--- gscvbackend/cards/models.py
+++ gscvbackend/cards/models.py
@@ -4,8 +4,16 @@ from django.db import models
 
 
 class Card(models.Model):
-  name = models.CharField(max_length=200)
+  public = models.BooleanField(default=True)
+  title = models.CharField(max_length=200, default="A Title !")
+  name = models.CharField(max_length=200, null=True)
   created_at = models.DateTimeField(auto_now_add=True)
+  color = models.CharField(max_length=200, null=False, default="#FF0066")
+  image = models.CharField(max_length=8192, null=True)
+  imageCoordinates = models.CharField(max_length=100, null=True)
+  imageSize = models.CharField(max_length=10, null=True)
+  radius = models.IntegerField(null=True)
+  fields = models.CharField(max_length=2048, null=True)
   
   def __unicode__(self):
-    return self.name
+    return self.title
